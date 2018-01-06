3b1d2d8ce888631be078ef67f0b97e3de539a43d
tatyuki1209 <tatyuki1209@gmail.com>
2018-01-06 13:40:34 +0700

    Update Listing Product Admin and CSS

--- TechLine-war/src/java/utils/PageProduct.java
+++ TechLine-war/src/java/utils/PageProduct.java
@@ -25,9 +25,9 @@ public class PageProduct {
     private final List<?> origModel;
     private List<?> model;
    
-    public PageProduct(List<?> model) {
+    public PageProduct(List<?> model, int addRecoredPage) {
         this.origModel = model;
-        this.records = DEFAULT_RECORDS_NUMBER;
+        this.records = addRecoredPage;
         this.pageIndex = DEFAULT_PAGE_INDEX;        
         this.recordsTotal = model.size();
 
@@ -62,7 +62,7 @@ public class PageProduct {
             toIndex = this.recordsTotal;
         }
 
-        this.model = (List<ProductIndexModel>) origModel.subList(fromIndex, toIndex);
+        this.model = (List<?>) origModel.subList(fromIndex, toIndex);
     }
 
     public void next() {
@@ -83,6 +83,10 @@ public class PageProduct {
 
     public int getRecords() {
         return records;
+    }
+
+    public void setRecords(int records) {
+        this.records = records;
     }
 
     public int getRecordsTotal() {
